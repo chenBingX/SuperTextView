dbde5083d96f95b430afd1f548f2ff604ecc06cb
Your Name <you@example.com>
2018-05-13 09:19:23 +0800

    1. 修复应用切换后台，当再次回到应用时，Adjuster动画可能不会播放的问题。

--- library/src/main/java/com/coorchice/library/SuperTextView.java
+++ library/src/main/java/com/coorchice/library/SuperTextView.java
@@ -1475,7 +1475,7 @@ public class SuperTextView extends TextView {
   @Override
   protected void onWindowVisibilityChanged(int visibility) {
     super.onWindowVisibilityChanged(visibility);
-    if (visibility != VISIBLE) {
+    if (visibility != VISIBLE && visibility != INVISIBLE) {
       cacheRunnableState = runnable;
       cacheNeedRunState = needRun;
       stopAnim();
